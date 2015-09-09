22c5c1b46db78f600c1483699b9e37114c818794
Jeremy Nicola <jeremy.nicola@gmail.com>
2015-09-10 01:02:43 +0200

    Archimedes forces varies linearly with the non-immerged radius of the robot, it improves the convergence

--- eurathlon2015-underwater/src/eurathlon/robots/saucisse.py
+++ eurathlon2015-underwater/src/eurathlon/robots/saucisse.py
@@ -14,12 +14,13 @@ class Saucisse(morse.core.robot.Robot):
     # mf : mass of the fluid displaced by the robot, used for Archimedes thrust, defaults to
     # the mass displaced by a cylinder of 20cm diameter, 1meter in length with a
     # water volumic mass of 1030 kg.m-3
-    def __init__(self, obj, parent=None, sea_level=0, lever_arm_length=1, mf=(0.1**2)*3.14*1*1030):
+    def __init__(self, obj, parent=None, sea_level=0, lever_arm_length=1, mf=(0.1**2)*3.14*1*1030, radius=0.1):
         logger.info('%s initialization' % obj.name)
         morse.core.robot.Robot.__init__(self, obj, parent)
         self._sea_level=sea_level
         self._lever_arm_length=lever_arm_length
         self._mf=mf
+        self._radius=radius
         logger.info('Component initialized')
 
     def default_action(self):
@@ -28,8 +29,12 @@ class Saucisse(morse.core.robot.Robot):
 
         ## Compute an approximation of Archimedes force
         # If robot is underwater, apply Archimedes force
-        if self.position_3d.z - self._sea_level <0:
-            force[2]+=1.1*self.bge_object.mass*9.81 #self._mf*9.81
+        # if robot is partially underwater, assumes Archimedes force varies linearly with its immersion
+        delta = self.position_3d.z - self._sea_level
+        if  delta <self._radius and delta > -self._radius:
+            force[2]+=self.bge_object.mass*9.81*(1-delta/self._radius)/2
+        elif self.position_3d.z - self._sea_level < -self._radius:
+            force[2]+=1.1*self.bge_object.mass*9.81
 
         self.bge_object.applyForce(force,True)
         self.bge_object.applyTorque(torque,True)
