80623daf1b39775fd58af88d6d0ec62e37714334
Jeremy Nicola <jeremy.nicola@gmail.com>
2015-09-10 00:33:25 +0200

    Simulating Archimedes force, but got nasty oscillations near the surface

--- eurathlon2015-underwater/src/eurathlon/robots/saucisse.py
+++ eurathlon2015-underwater/src/eurathlon/robots/saucisse.py
@@ -4,12 +4,32 @@ import morse.core.robot
 class Saucisse(morse.core.robot.Robot):
     _name = 'saucisse robot'
 
-    def __init__(self, obj, parent=None):
+    _u1 = 0 # Left thruster, looking forward (positive means turns right)
+    _u2 = 0 # Right thruster, looking forward (positive means turns left)
+    _u3 = 0 # Z-thruster, looking up (positive means go up)
+
+    ##
+    # sea_level : for example if the map is 1m too high, sea_level = 1
+    # lever_arm_length : distance between a left/right thruster to the center of mass
+    # mf : mass of the fluid displaced by the robot, used for Archimedes thrust, defaults to
+    # the mass displaced by a cylinder of 20cm diameter, 1meter in length with a
+    # water volumic mass of 1030 kg.m-3
+    def __init__(self, obj, parent=None, sea_level=0, lever_arm_length=1, mf=(0.1**2)*3.14*1*1030):
         logger.info('%s initialization' % obj.name)
         morse.core.robot.Robot.__init__(self, obj, parent)
-
-        # Do here robot specific initializations
+        self._sea_level=sea_level
+        self._lever_arm_length=lever_arm_length
+        self._mf=mf
         logger.info('Component initialized')
 
     def default_action(self):
-        pass
+        force=[self._u1+self._u2,0,self._u3]
+        torque=[0,0,self._u2*self._lever_arm_length-self._u1*self._lever_arm_length]
+
+        ## Compute an approximation of Archimedes force
+        # If robot is underwater, apply Archimedes force
+        if self.position_3d.z - self._sea_level <0:
+            force[2]+=1.1*self.bge_object.mass*9.81 #self._mf*9.81
+
+        self.bge_object.applyForce(force,True)
+        self.bge_object.applyTorque(torque,True)
