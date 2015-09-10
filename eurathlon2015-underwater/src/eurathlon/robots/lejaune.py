3193aa64add91b47bc5cda03cc54d302f52d443d
Jeremy Nicola <jeremy.nicola@gmail.com>
2015-09-10 12:22:06 +0200

    Physically acurate Lejaune robot

--- eurathlon2015-underwater/src/eurathlon/robots/lejaune.py
+++ eurathlon2015-underwater/src/eurathlon/robots/lejaune.py
@@ -1,8 +1,15 @@
 import logging; logger = logging.getLogger("morse." + __name__)
 import morse.core.robot
+from morse.helpers.components import add_property
+from math import cos,sin,radians
 
 class Lejaune(morse.core.robot.Robot):
+    add_property('_lever_arm_length',0.3,'LeverArmLength','float','Distance from the center of mass of the boat to the thruster')
+
     _name = 'lejaune robot'
+
+    _u1 = 0 # Thruster force, positive means the boat goes forward
+    _u2 = 0 # Thruster orientation, in degrees. 0 is neutral, 90 means the boat turns right, -90 means the boat turns left (the angle represents the rudder-stick angle with the x-axis of the robot.)
 
     def __init__(self, obj, parent=None):
         logger.info('%s initialization' % obj.name)
@@ -12,5 +19,5 @@ class Lejaune(morse.core.robot.Robot):
         logger.info('Component initialized')
 
     def default_action(self):
-        self.bge_object.applyForce([0,0,200])
-        pass
+        self.bge_object.applyForce([self._u1*cos(radians(self._u2)),self._u1*sin(radians(self._u2)),0],True)
+        self.bge_object.applyTorque([0,0,self._u1*sin(radians(self._u2))],True)
