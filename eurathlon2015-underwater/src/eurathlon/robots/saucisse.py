23c5e27ceaa46523fa902c3f9cb8b4b9f9c916ca
Jeremy Nicola <jeremy.nicola@gmail.com>
2015-09-10 02:33:07 +0200

    Using nice add_property feature to set properties of SAUCISSE

--- eurathlon2015-underwater/src/eurathlon/robots/saucisse.py
+++ eurathlon2015-underwater/src/eurathlon/robots/saucisse.py
@@ -1,8 +1,14 @@
 import logging; logger = logging.getLogger("morse." + __name__)
 import morse.core.robot
 from math import acos, sqrt
+from morse.helpers.components import add_property
 
 class Saucisse(morse.core.robot.Robot):
+    add_property('_sea_level',0,'SeaLevel','float','Indicates the level of the sea. For example, if the mesh representing the sea is 0.7m above 0, set _sea_level=0')
+    add_property('_lever_arm_length',1,'LeverArmLength','float','Distance from a thruster to the center of gravity of the robot. Does not apply to the vertical thruster')
+    add_property('_mf',(0.1**2)*3.14*1*1030,'MF','float','Volume of the robot. Used to simulate Archimedes force.')
+    add_property('_radius',0.1,'Radius','float','Radius of the cylinder of the robot in meter.')
+
     _name = 'saucisse robot'
 
     _u1 = 0 # Left thruster, looking forward (positive means turns right)
@@ -15,14 +21,11 @@ class Saucisse(morse.core.robot.Robot):
     # mf : mass of the fluid displaced by the robot, used for Archimedes thrust, defaults to
     # the mass displaced by a cylinder of 20cm diameter, 1meter in length with a
     # water volumic mass of 1030 kg.m-3
-    def __init__(self, obj, parent=None, sea_level=0, lever_arm_length=1, mf=(0.1**2)*3.14*1*1030, radius=0.1):
+    def __init__(self, obj, parent=None):
         logger.info('%s initialization' % obj.name)
         morse.core.robot.Robot.__init__(self, obj, parent)
-        self._sea_level=sea_level
-        self._lever_arm_length=lever_arm_length
-        self._mf=mf
-        self._radius=radius
         logger.info('Component initialized')
+        print(self._sea_level)
 
     def default_action(self):
         force=[self._u1+self._u2,0,self._u3]
@@ -35,9 +38,9 @@ class Saucisse(morse.core.robot.Robot):
         if  delta <self._radius and delta > -self._radius:
             h_imm=self._radius - delta
             vol_imm=self._radius**2*acos((self._radius-h_imm)/self._radius)-(self._radius-h_imm)*sqrt(2*self._radius*h_imm-h_imm**2)*1/(0.1**2*3.14*1)
-            force[2]+=1.05*self.bge_object.mass*9.81
+            force[2]+=self.bge_object.mass*9.81
         elif self.position_3d.z - self._sea_level < -self._radius:
-            force[2]+=1.05*self.bge_object.mass*9.81
+            force[2]+=1.01*self.bge_object.mass*9.81
 
         self.bge_object.applyForce(force,True)
         self.bge_object.applyTorque(torque,True)
