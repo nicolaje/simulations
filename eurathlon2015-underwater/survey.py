5ead67f5be4be9911ab401e2f3170546bbc680c3
Jeremy Nicola <jeremy.nicola@gmail.com>
2015-09-13 22:10:00 +0200

    Current state of the survey

--- eurathlon2015-underwater/survey.py
+++ eurathlon2015-underwater/survey.py
@@ -8,15 +8,25 @@ from eurathlon.builder.sensors import Tritechmicron
 from morse.builder import *
 
 jaune = Lejaune()
-jaune.translate(z=0.7)
-#ir = Infrared()
-#ir.properties(laser_range=50)
-#jaune.append(ir)
+jaune.translate(x=-150,y=300,z=0.7)
+
 tritech=Tritechmicron()
-tritech.translate(z=2)
 tritech.rotate(z=3.14/2)
 jaune.append(tritech)
 
+pose = Pose()
+jaune.append(pose)
+
+velocity = Velocity()
+jaune.append(velocity)
+
+jauneactuator=Lejaunethruster()
+jaune.append(jauneactuator)
+
+jaune.add_default_interface('socket')
+
 env = Environment('../data/eurathlon/environment/map.blend', fastmode = False)
-env.set_camera_location([-18.0, -6.7, 10.8])
-env.set_camera_rotation([1.09, 0, -1.14])
+env.set_camera_location([-18.0-150, -6.7+330, 10.8])
+env.set_camera_rotation([1.09, 0, -3*3.14/4])
+env.set_camera_clip(clip_start=0.1, clip_end=2000)
+env.set_camera_speed(speed=4)
