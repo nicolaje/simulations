eec1dbaf68ca85d74d894cded8f49953505b4687
Jeremy Nicola <jeremy.nicola@gmail.com>
2015-09-13 19:49:34 +0200

    Satisfying Tritech Micron sensor

--- eurathlon2015-underwater/src/eurathlon/builder/sensors/tritechmicron.py
+++ eurathlon2015-underwater/src/eurathlon/builder/sensors/tritechmicron.py
@@ -9,12 +9,12 @@ class Tritechmicron(LaserSensorWithArc):
 
     def __init__(self, name=None):
         LaserSensorWithArc.__init__(self, name)
-        mesh = Cube("InfraredCube")
+        mesh = Cube("TritechCube")
         mesh.scale = (.02, .02, .02)
         mesh.color(.8, .8, .8)
         self.append(mesh)
         # set components-specific properties
-        self.properties(Visible_arc = True, laser_range = 2.0,
+        self.properties(Visible_arc = True, laser_range = 40.0,
                 scan_window = 20.0, resolution = 1.0)
         # set the frequency to 10 Hz
         self.frequency(10)
--- eurathlon2015-underwater/src/eurathlon/sensors/tritechmicron.py
+++ eurathlon2015-underwater/src/eurathlon/sensors/tritechmicron.py
@@ -6,6 +6,9 @@ from morse.core.services import service, async_service
 from morse.core import status
 from morse.helpers.components import add_data, add_property
 from morse.sensors.laserscanner import LaserScanner
+from morse.builder import bpymorse
+from math import radians
+import bpy
 
 class Tritechmicron(LaserScanner):
     """Write here the general documentation of your sensor.
@@ -14,44 +17,59 @@ class Tritechmicron(LaserScanner):
 
     _name = "Tritechmicron"
     _short_desc = "Simulator for the Tritech Micron sonar"
-    -rot = 0
+    _rot = 0
 
-    # define here the data fields exported by your sensor
-    # format is: field name, default initial value, type, description
-    add_data('distance', 0.0, 'float', 'A dummy odometer, for testing purposes. Distance in meters')
-    add_data('color', 'none', 'str', 'A dummy colorimeter, for testing purposes. Default to \'none\'.')
+    add_data('range', 0.0, 'float', 'Range detected by the sonar. It is the mean range of the range_list data.')
+    add_data('incidence', 0.0, 'float', 'Incidence of the beam that measured the range. ')
+
+    add_property('angle_per_ping',9,'angle_per_ping',"float","The angle jump between two pings, in degrees.")
+    add_property('ping_per_second',7,'ping_per_second',"float","The number of ping per second.")
+    add_property('scan_coverage',90,'scan_coverage',"float","The width of the zone to be covered by the sonar, in degrees.")
 
     def __init__(self, obj, parent=None):
         logger.info("%s initialization" % obj.name)
         # Call the constructor of the parent class
         LaserScanner.__init__(self, obj, parent)
+        self.bge_object.applyRotation([0,radians(90-self.scan_coverage/2),0],True)
+        #arcMat=None
+        #if 'RayMat' in bpymorse.get_materials():
+        #    m = bpymorse.get_material('RayMat')
+        #    print(str(type(bpymorse.get_material('RayMat'))))
+        #    print(str(dir(bpymorse.get_material('RayMat'))))
+        #    print("m.game_settings.use_backface_culling: "+str(m.game_settings.use_backface_culling))
+        #    m.game_settings.use_backface_culling= False
+        #    print("m.game_settings.use_backface_culling: "+str(m.game_settings.use_backface_culling))
+        #    arcMat= m
+        #m.diffuse_color=(0,0,1)
+        #m.use_transparency=False
+        #arcName = ""
+        #for child in obj.children:
+        #    print(child.name)
+        #    if "Arc_" in child.name:
+        #        print(child.name + "contains Arc_")
+        #        arcName=child.name
+        #arcObj=bpymorse.get_object(arcName)
+        #bpymorse.select_only(arcObj)
+        #bpy.ops.object.modifier_add(type='SOLIDIFY')
+        #print("str(dir(arcObj)): "+str(dir(arcObj)))
+        #print("str(dir(arcObj.data)): "+str(dir(arcObj.data)))
+        #print("arcObj.data.show_double_sided : "+str(arcObj.data.show_double_sided))
+        #print("self.local_data.keys(): "+str(self.local_data.keys()))
+        #        print(str(dir(child)))
+        #        print(dir(child.meshes))
+        #        print(len(child.meshes))
+        #        print(str(dir(child.meshes[0])))
+        #        print(child.meshes[0].getMaterialName(0))
+        #        print(len(child.meshes[0].materials))
+        #        #print(child.meshes[0].materials.name)
+        #        print(str(dir(child.meshes[0].materials[0])))
 
-        # Do here sensor specific initializations
-
-        self._distance = 0 # dummy internal variable, for testing purposes
-
-        logger.info('Component initialized')
-        print("SALUT C'EST COOL")
-
-    @service
-    def get_current_distance(self):
-        """ This is a sample (blocking) service (use 'async_service' decorator
-        for non-blocking ones).
-
-        Simply returns the value of the internal counter.
-
-        You can access it as a RPC service from clients.
-        """
-        logger.info("%s is %sm away" % (self.name, self.local_data['distance']))
-
-        return self.local_data['distance']
-
-    """Used for Velodyne sensor"""
     def default_action(self):
         LaserScanner.default_action(self)
-        self.applyRotationZ()
-
-
-    def applyRotationZ(self, rz=.01745):
-        # The second parameter specifies a "local" movement
-        self.bge_object.applyRotation([0, rz, 0], True)
+        self.local_data['range']=min(self.local_data['range_list'])
+        self.local_data['incidence']=self._rot
+        self.bge_object.applyRotation([0,radians(self.angle_per_ping),0],True)
+        self._rot+=self.angle_per_ping
+        if self._rot > self.scan_coverage :
+            self.bge_object.applyRotation([0,-radians(self._rot),0],True)
+            self._rot = 0
