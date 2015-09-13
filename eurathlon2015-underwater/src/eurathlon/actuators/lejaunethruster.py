e24e08013a75d6a8082c2a5b56e1101605480dd3
Jeremy Nicola <jeremy.nicola@gmail.com>
2015-09-13 20:38:56 +0200

    Satisfying LeJaune actuator

--- eurathlon2015-underwater/src/eurathlon/actuators/lejaunethruster.py
+++ eurathlon2015-underwater/src/eurathlon/actuators/lejaunethruster.py
@@ -7,59 +7,15 @@ from morse.core import status
 from morse.helpers.components import add_data, add_property
 
 class Lejaunethruster(morse.core.actuator.Actuator):
-    """Write here the general documentation of your actuator.
-    It will appear in the generated online documentation.
-    """
     _name = "Lejaunethruster"
     _short_desc = "Propulseur pour le robot "
 
-    # define here the data fields required by your actuator
-    # format is: field name, initial value, type, description
-    add_data('counter', 0, 'int', 'A dummy counter, for testing purposes')
+    add_data('u1', 0, 'float', 'Thruster force, positive means the boat goes forward')
+    add_data('u2', 0, 'float', 'Thruster orientation, in degrees. 0 is neutral, 90 means the boat turns right, -90 means the boat turns left (the angle represents the rudder-stick angle with the x-axis of the robot.)')
 
     def __init__(self, obj, parent=None):
-        logger.info("%s initialization" % obj.name)
-        # Call the constructor of the parent class
         morse.core.actuator.Actuator.__init__(self, obj, parent)
 
-        # Do here actuator specific initializations
-
-        self._target_count = 0 # dummy internal variable, for testing purposes
-
-        logger.info('Component initialized')
-
-    @service
-    def get_counter(self):
-        """ This is a sample service.
-
-        Simply returns the value of the internal counter.
-
-        You can access it as a RPC service from clients.
-        """
-        logger.info("%s counter is %s" % (self.name, self.local_data['counter']))
-
-        return self.local_data['counter']
-
-    @interruptible
-    @async_service
-    def async_test(self, value):
-        """ This is a sample asynchronous service.
-
-        Returns when the internal counter reaches ``value``.
-
-        You can access it as a RPC service from clients.
-        """
-        self._target_count = value
-
     def default_action(self):
-        """ Main loop of the actuator.
-
-        Implements the component behaviour
-        """
-
-        # check if we have an on-going asynchronous tasks...
-        if self._target_count and self.local_data['counter'] > self._target_count:
-            self.completed(status.SUCCESS, self.local_data['counter'])
-
-        # implement here the behaviour of your actuator
-        self.local_data['counter'] += 1
+        self.robot_parent._u1=self.local_data['u1']
+        self.robot_parent._u2=self.local_data['u2']
